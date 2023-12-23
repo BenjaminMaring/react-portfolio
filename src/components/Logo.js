9a78d30a7f6298f1e89fd0c881dacc0929dece98
BenjaminMaring <benjammaring@gmail.com>
2023-12-23 00:01:27 -0800

    see dev log 12/23

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import './css/Logo.css'
 export default function Logo() {
     return (
         <div className="logo-wrapper">
-            <a href=""><img className="logo-top" alt="logo" src="./images/logo-top.png"/></a>
+            <a href="http://localhost:3000"><img className="logo-top" alt="logo" src="./images/logo-top.png"/></a>
             <img alt="logo" src="./images/logo-bottom.png"/>
         </div>
     )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import './css/Sidebar.css';
+import { motion } from "framer-motion"
 
-function Sidebar() {
+export default function Sidebar() {
     return (
         <nav className="sidebar-wrapper">
-            <div className="sidebar-lineBefore"></div>
-            <div className="sidebar-socials">
-                <a className="sidebar-1" href="https://github.com/BenjaminMaring"><i class="fa-brands fa-github"></i></a>
-                <a className="sidebar-2"href="https://www.linkedin.com/in/benjamin-maring-453637216/"><i class="fa-brands fa-linkedin-in"></i></a>
-                <a className="sidebar-3" href="https://discord.com/users/xlnvis#5333"><i class="fa-brands fa-discord"></i></a>
-                <a className="sidebar-4" href="mailto:maringben@hotmail.com"><i class="fa-regular fa-envelope"></i></a>
-            </div>
-            <div className="sidebar-lineAfter"></div>
+            <motion.div className="sidebar-lineBefore"
+                animate={{height: 200}}
+                transition={{ease: "linear", duration: 1.2}}
+            ></motion.div>
+
+
+            <motion.div className="sidebar-socials" id="sidebar-social-wrapper"
+                animate={{height: 150, margin: 10}}
+                transition={{ease: "easeOut", duration: 1.2, delay: 1.2}}
+            >
+                <a className="sidebar-1" href="https://github.com/BenjaminMaring"><i className="fa-brands fa-github"></i></a>
+                <a className="sidebar-2"href="https://www.linkedin.com/in/benjamin-maring-453637216/"><i className="fa-brands fa-linkedin-in"></i></a>
+                <a className="sidebar-3" href="https://codepen.io/BenjaminMaring"><i className="fa-brands fa-codepen"></i></a>
+                <a className="sidebar-4" href="mailto:maringben@hotmail.com"><i className="fa-regular fa-envelope"></i></a>
+            </motion.div>
+
+
+            <motion.div className="sidebar-lineAfter"
+                animate={{height: 200}}
+                transition={{ease: "linear", duration: 1.2}}
+            ></motion.div>
         </nav>
     )
 }
-
-export default Sidebar;
