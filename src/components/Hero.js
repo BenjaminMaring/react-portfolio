15c0f21f92b94f38d6b951ed87d8f237bbe1a596
BenjaminMaring <benjammaring@gmail.com>
2023-12-26 12:48:35 -0800

    changes to hero elem

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,11 @@ import './css/Hero.css'
 function Hero() {
     return (
         <div className="hero-wrapper">
-            <h1><span data-string="Front" className="delay-one">Front</span> <span data-string="End" className="delay-two">End</span><br />
-                <span data-string="Software" className="delay-three">Software</span> <span data-string="Engineer" className="delay-four">Engineer</span></h1>
-            <h5>Hello, my name is <span className="hero-accent">Benjamin Maring</span>,</h5>
-            <h6>and I am an aspiring full stack developer. Currently I am learning web developement as an intern at <a className="hero-bvt" href="https://www.bayvalleytech.com">Bay Valley Tech</a>, as well as a technician at a small MSP doing a wide variety of tasks.</h6>
+            <h5>Hello, my name is <span className="hero-accent">Benjamin Maring</span>, a</h5>
+            <h1><span data-string="Front" className="delay-one hero-first">Front</span> <span data-string="End" className="delay-two hero-first">End</span></h1>
+            <h1><span data-string="Software" className="delay-three">Software</span> <span data-string="Engineer" className="delay-four">Engineer</span></h1>
+            <h6>Currently I am learning web developement as an intern at <a className="hero-bvt" href="https://www.bayvalleytech.com">Bay Valley Tech</a>, as well as a technician at a small MSP doing a wide variety of tasks.</h6>
+            <button className="hero-button" onClick={() => console.log("clicked")}>View My Projects</button>
         </div>
     );
 }
